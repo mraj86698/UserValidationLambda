0c28eda78d6a43bcaf7acb35dc4913c87ac0f95d
MohanRaj <mraj86698@gmail.com>
2022-08-25 15:51:13 +0530

    Added to Welcome Message

---
+++ UserValidationLambda/src/main/java/com/java/maven/UserValidationLambda/UserValidator.java
@@ -0,0 +1,13 @@
+package com.java.maven.UserValidationLambda;
+
+/**
+ * Hello world!
+ *
+ */
+public class UserValidator
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "User Validation Using Lambda Expression!" );
+    }
+}
---
+++ UserValidationLambda/src/test/java/com/java/maven/UserValidationLambda/UserValidatorTest.java
@@ -0,0 +1,20 @@
+package com.java.maven.UserValidationLambda;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class UserValidatorTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
